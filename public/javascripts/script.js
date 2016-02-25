21ec4aa9311b01606de5cb612fb62702a3f88ca6
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-02-26 03:35:45 +0900

    fix handsontable script

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,15 +1,24 @@
 $(function () {
     var data = [
-        ["", "Ford", "Volvo", "Toyota", "Honda"],
-        ["2016", 10, 11, 12, 13],
-        ["2017", 20, 11, 14, 13],
-        ["2018", 30, 15, 12, 13]
+        {id: 1, name: {first: 'Ted', last: 'Right'}},
+        {id: 2, name: {first: 'Frank', last: 'Honest'}},
+        {id: 3, name: {first: 'Joan', last: 'Well'}},
+        {id: 4, name: {first: 'Gail', last: 'Polite'}},
+        {id: 5, name: {first: 'Michael', last: 'Fair'}}
     ];
 
     var container = document.getElementById('example');
     var hot = new Handsontable(container, {
         data: data,
-        rowHeaders: true,
-        colHeaders: true
+        rowHeaders: false,
+        colHeaders: ['ID', 'FIRST NAME', 'LAST NAME'],
+        minSpareRows: 1,
+        startRows: 5,
+        startCols: 3,
+        columns: [
+            {data: 'id'},
+            {data: 'name.first'},
+            {data: 'name.last'}
+        ]
     });
 });
