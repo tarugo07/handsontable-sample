927614755b0a0d0406e6192b64abe720370f1dad
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-02-28 03:21:35 +0900

    move handsontable script

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,14 +8,27 @@ case class User(id: Int, firstName: String, lastName: String)
 
 class Application extends Controller {
 
-  implicit val reads = (
-    (__ \ 'id).read[Int] and
-      (__ \ 'name \ 'first).read[String] and
-      (__ \ 'name \ 'last).read[String]
-    )((id, firstName, lastName) => User(id, firstName, lastName))
+  implicit val reads: Reads[User] = (
+    (__ \ "id").read[Int] and
+      (__ \ "name" \ "first").read[String] and
+      (__ \ "name" \ "last").read[String]
+    )(User.apply _) //((id, firstName, lastName) => User(id, firstName, lastName))
+
+  implicit val writers: Writes[User] = (
+    (__ \ "id").write[Int] and
+      (__ \ "name" \ "first").write[String] and
+      (__ \ "name" \ "last").write[String]
+    )(unlift(User.unapply)) //(user => (user.id, user.firstName, user.lastName))
 
   def index = Action {
-    Ok(views.html.index())
+    val users = Seq(
+      User(id = 1, firstName = "Ted", lastName = "Right"),
+      User(id = 2, firstName = "Frank", lastName = "Honest"),
+      User(id = 3, firstName = "Joan", lastName = "Well"),
+      User(id = 4, firstName = "Gail", lastName = "Polite"),
+      User(id = 5, firstName = "Michael", lastName = "Fair")
+    )
+    Ok(views.html.index(Json.toJson(users).toString()))
   }
 
   def save = Action(parse.json) { implicit request =>
@@ -24,6 +37,7 @@ class Application extends Controller {
         users.foreach(println)
         Ok(Json.obj("status" -> "OK"))
     }.recoverTotal { e =>
+      println(JsError.toFlatForm(e))
       BadRequest(Json.obj("status" -> "NG"))
     }
   }
